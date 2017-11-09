10fa27d3aa459a325df62b5fbffda95669f3a0b3
Bobby Siagian <bobbysiagian@yahoo.com>
2017-11-09 14:49:18 +0700

    sample read html/file with node and define sample url to access the date value

--- firstModule.js
+++ firstModule.js
@@ -2,6 +2,7 @@ var http = require('http');
 var dt = require('./date'); //call module
 var url = require('url');
 
+//access using url sample : http://localhost:8080/?year=2017&month=July
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     //res.write("The date and time are currently: " + dt.myDateTime());
